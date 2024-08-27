461d1226cffb6bc5d36ebc5ec45a9a2f013b4d89
Shravankumar623 <sharptechshravankumar@gmail.com>
2024-08-27 13:11:23 +0530

    Commit by Shravankumar Changed Empoyee Model added one Annotation

--- sharptechinternal/src/main/java/com/sharp/model/Employee.java
+++ sharptechinternal/src/main/java/com/sharp/model/Employee.java
@@ -8,7 +8,14 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
@@ -282,6 +289,7 @@ public class Employee implements UserDetails {
 	private String role;
 
 	@Override
+	@JsonIgnore
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
 		return List.of(new SimpleGrantedAuthority(role));
